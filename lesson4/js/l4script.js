1c9f9b48ad7219c98268bb5dc09dc867d4e4dafa
John Doe <shandaaurich@hotmail.com>
2021-01-27 21:53:00 -0800

    fixed date on footer and centered small nav

--- lesson4/js/l4script.js
+++ lesson4/js/l4script.js
@@ -6,9 +6,15 @@ document.getElementById("year").textContent = new Date().toLocaleDateString(
   "en-US",
   year
 );
-document.getElementById("date").textContent = date;
-console.log(date);
 
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+
+const datefield = document.querySelector("date");
+
+datefield.textContent = fulldate;
 function toggleMenu() {
     
   document.getElementById("toggleNav").classList.toggle("hide");
