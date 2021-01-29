1c32a5f97cbbc3fbb24699774b800481ae87a7fc
John Doe <shandaaurich@hotmail.com>
2021-01-29 12:02:34 -0800

    added preston-5 page

---
+++ lesson5/js/script.js
@@ -0,0 +1,19 @@
+const year = { year: "numeric" };
+
+document.getElementById("year").textContent = new Date().toLocaleDateString(
+  "en-US",
+  year
+);
+
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+  now
+);
+
+const datefield = document.getElementById("date");
+
+datefield.textContent = fulldate;
+
+function toggleMenu() {
+  document.getElementById("toggleNav").classList.toggle("hide");
+}
