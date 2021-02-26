be5088cad8c76bb0dfd7f957038f5c67f8d3c29f
John Doe <shandaaurich@hotmail.com>
2021-02-26 10:46:01 -0800

    fixing footer javascript

--- lesson7/javascript/javascript.js
+++ lesson7/javascript/javascript.js
@@ -32,3 +32,22 @@ realImages.forEach(image => {
   imageObserver.observe(image);
 })
 
+//footer year
+
+const year = { year: "numeric" };
+
+document.getElementById("year").textContent = new Date().toLocaleDateString(
+  "en-US",
+  year
+);
+
+//current date footer
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+  now
+);
+
+const datefield = document.getElementById("date");
+
+datefield.textContent = fulldate;
+
---
+++ lesson8/javascript.js
@@ -0,0 +1,18 @@
+//footer year
+
+const year = { year: "numeric" };
+
+document.getElementById("year").textContent = new Date().toLocaleDateString(
+  "en-US",
+  year
+);
+
+//current date footer
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+  now
+);
+
+const datefield = document.getElementById("date");
+
+datefield.textContent = fulldate;
