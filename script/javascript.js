18bf03237fd9623dcc43fd3c845f4217f5a70be5
John Doe <shandaaurich@hotmail.com>
2021-02-26 23:12:40 -0800

    reconnected js on storm center

--- script/javascript.js
+++ script/javascript.js
@@ -8,3 +8,15 @@ document.getElementById("year").textContent = new Date().toLocaleDateString(
 );
 document.getElementById("date").textContent = date;
 console.log(date);
+
+function adjustRating(rating) {
+  document.getElementById("ratingvalue").innerHTML = rating;
+}
+
+function selectRegion() {
+	const s = document.querySelector('#selected')
+	const sel = document.querySelector('#stormRegion');
+	s.style.display = "block";
+	s.textContent = sel.value;
+	
+}
