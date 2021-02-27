1461f08e1a7481eacf9080f16e21c6b78b5ca178
John Doe <shandaaurich@hotmail.com>
2021-02-26 23:17:40 -0800

    fixed javascript hambutton

--- script/javascript.js
+++ script/javascript.js
@@ -1,22 +1,22 @@
-const year = { year: "numeric" };
+// const year = { year: "numeric" };
 
-const date = document.lastModified;
+// const date = document.lastModified;
 
-document.getElementById("year").textContent = new Date().toLocaleDateString(
-  "en-US",
-  year
-);
-document.getElementById("date").textContent = date;
-console.log(date);
+// document.getElementById("year").textContent = new Date().toLocaleDateString(
+//   "en-US",
+//   year
+// );
+// document.getElementById("date").textContent = date;
+// console.log(date);
 
-function adjustRating(rating) {
-  document.getElementById("ratingvalue").innerHTML = rating;
-}
+// function adjustRating(rating) {
+//   document.getElementById("ratingvalue").innerHTML = rating;
+// }
 
-function selectRegion() {
-	const s = document.querySelector('#selected')
-	const sel = document.querySelector('#stormRegion');
-	s.style.display = "block";
-	s.textContent = sel.value;
+// function selectRegion() {
+// 	const s = document.querySelector('#selected')
+// 	const sel = document.querySelector('#stormRegion');
+// 	s.style.display = "block";
+// 	s.textContent = sel.value;
 	
-}
+// }
