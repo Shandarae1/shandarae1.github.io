55e073a3602f96c459dca345b7fe8cb24a8654c5
John Doe <shandaaurich@hotmail.com>
2021-03-02 19:58:17 -0800

    Image cards on Index

--- lesson9/javascript/javascript.js
+++ lesson9/javascript/javascript.js
@@ -36,3 +36,39 @@ card.appendChild(image);
 
 });
 
+// ---------Lazy Load Images---------
+
+// Lazy loading images
+
+const realImages = document.querySelectorAll("[data-src]");
+
+function imgLoad(img) {
+  const source = img.getAttribute("data-src");
+  if(!source) {
+    return;
+  }
+  img.src = source;
+  img.removeAttribute("data-src");
+}
+
+const imageOptions = {
+  threshold: 1,
+  rootMargin: "0px 0px 50px 0px"
+};
+
+const imageObserver = new IntersectionObserver((items, imageObserver) => {
+  items.forEach(item => {
+    if(!item.isIntersecting){
+      return;
+    } 
+    else {
+      imgLoad(item.target);
+      imageObserver.unobserve(item.target);
+    }
+  })
+  }, imageOptions);
+
+realImages.forEach(image => {
+  imageObserver.observe(image);
+})
+
