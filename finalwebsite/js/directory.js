da72ea3a774ce41c8694dd82a82d795b274a3f6a
John Doe <shandaaurich@hotmail.com>
2021-03-26 14:02:13 -0700

    json for events

--- finalwebsite/js/directory.js
+++ finalwebsite/js/directory.js
@@ -1,51 +1,50 @@
-// ------HOMEPAGE JSON FETCH------
-  
-  // ------storing resource------
-  
-  const reqURL = "JSON/chamber.json";
-  
-  // -----Fetch-----
-  
-  fetch(reqURL)
-    .then(function (companyresponse) {
-      return companyresponse.json();
-    })
-    .then(function (jsonObject) {
-      //   -----store converted results into array -----
-  
-      // ------town cards------
-  
-      const companies = jsonObject["Companies"];
-      const directory = document.querySelector("div.indexCards");
+// ------DIRECTORY JSON FETCH------
 
-      console.log(companies);
 
-      companies.forEach((company) => {
-          let companycard = document.createElement("secion");
-          let companyarticle = document.createElement("article");
-          let companylogo = document.createElement("img");
-          let companyName = document.createElement("h2");
-          let companyAddress = document.createElement("p");
-          let companyCity = document.createElement("p");
-          let companyPhone = document.createElement("p");
-          let companyWeb = document.createElement("p");
-      
-          companyName.innerHTML = `${company.Name}`;
-          companyAddress.innerHTML = `${company.address}`;
-          companyCity.innerHTML = `${company.city}`;
-          companyPhone.innerHTML = `${company.phone}`;
-          companyWeb.innerHTML = `${company.weblink}`;
-          companylogo.setAttribute("src", `images/${company.logo}`);
-          companylogo.setAttribute("alt", `${company.name} logo`);
-          companylogo.setAttribute("loading", "lazy");
+const reqURL =
+  "https://raw.githubusercontent.com/Shandarae1/shandarae1.github.io/master/finalwebsite/JSON/chamber.json";
 
-          directory.append(companycard);
-          companycard.appendChild(companylogo);
-          companycard.appendChild(companyarticle);
-          companyarticle.appendChild(companyName);
-          companyarticle.appendChild(companyAddress);
-          companyarticle.appendChild(companyCity);
-          companyarticle.appendChild(companyPhone);
-          companyarticle.appendChild(companyWeb);
-        });
+// -----Fetch-----
+
+fetch(reqURL)
+  .then(function (companyresponse) {
+    return companyresponse.json();
+  })
+  .then(function (jsonObject) {
+
+    const companies = jsonObject["Companies"];
+    const directory = document.querySelector("div.cards");
+
+
+    companies.forEach((company) => {
+      let a = document.createElement("a");
+      let companycard = document.createElement("section");
+      let companyarticle = document.createElement("article");
+      let companylogo = document.createElement("img");
+      let companyName = document.createElement("h2");
+      let companyAddress = document.createElement("p");
+      let companyCity = document.createElement("p");
+      let companyPhone = document.createElement("p");
+      let companyWeb = document.createElement("p");
+
+      a.setAttribute("href", `${company.weblink}`)
+      companyName.innerHTML = `${company.Name}`;
+      companyAddress.innerHTML = `${company.address}`;
+      companyCity.innerHTML = `${company.city}`;
+      companyPhone.innerHTML = `${company.phone}`;
+      companyWeb.innerHTML = `${company.weblink}`;
+      companylogo.setAttribute("src", `images/${company.logo}`);
+      companylogo.setAttribute("alt", `${company.name} logo`);
+      companylogo.setAttribute("loading", "lazy");
+
+      directory.append(companycard);
+      companycard.appendChild(a);
+      a.appendChild(companyarticle);
+      companyarticle.appendChild(companyName);
+      companyarticle.appendChild(companylogo);
+      companyarticle.appendChild(companyAddress);
+      companyarticle.appendChild(companyCity);
+      companyarticle.appendChild(companyPhone);
+      companyarticle.appendChild(companyWeb);
     });
+  });
--- finalwebsite/js/indexjs.js
+++ finalwebsite/js/indexjs.js
@@ -26,6 +26,85 @@ function toggleMenu() {
   
   datefield.textContent = fulldate;
   
+  // ------ADS JSON FETCH------
+
+
+const reqURL =
+"https://raw.githubusercontent.com/Shandarae1/shandarae1.github.io/master/finalwebsite/JSON/chamber.json";
+
+// -----Fetch-----
+
+fetch(reqURL)
+.then(function (companyresponse) {
+  return companyresponse.json();
+})
+.then(function (jsonObject) {
+
+  const companies = jsonObject["Companies"];
+  const directory = document.querySelector("div.companyAds");
+
+  const companyfilter = companies.filter((companyAd) => companyAd.No === "1" || companyAd.No === "2" || companyAd.No === "5");
+
+  console.log(companyfilter);
+
+  companyfilter.forEach((company) => {
+    let a = document.createElement("a");
+    let companycard = document.createElement("section");
+    let companyarticle = document.createElement("article");
+    let companylogo = document.createElement("img");
+    let companyName = document.createElement("h2");
+    let companyWeb = document.createElement("p");
+
+    a.setAttribute("href", `${company.weblink}`)
+    companyName.innerHTML = `${company.Name}`;
+    companyWeb.innerHTML = `${company.weblink}`;
+    companylogo.setAttribute("src", `images/${company.logo}`);
+    companylogo.setAttribute("alt", `${company.name} logo`);
+    companylogo.setAttribute("loading", "lazy");
+
+    directory.append(companycard);
+    companycard.appendChild(a);
+    a.appendChild(companyarticle);
+    companyarticle.appendChild(companyName);
+    companyarticle.appendChild(companylogo);
+    companyarticle.appendChild(companyWeb);
+  });
+});
+
+const getURL = "https://raw.githubusercontent.com/Shandarae1/shandarae1.github.io/master/finalwebsite/JSON/chamber.json";
+
+fetch(getURL)
+.then(function (eventresponse) {
+  return eventresponse.json();
+})
+.then(function (jsnObject) {
+
+  const events = jsnObject["Events"];
+  const eventBox = document.querySelector("div.events");
+
+  console.log(events);
+
+  events.forEach((event) => {
+    let eventcard = document.createElement("section");
+    let eventh3 = document.createElement("h3");
+    let eventhr = document.createElement("hr");
+    let eventp1 = document.createElement("p");
+    let eventp2 = document.createElement("p");
+    let eventp3 = document.createElement("p");
+
+    eventh3.innerHTML = `${event.Title}`;
+    eventp1.innerHTML = `${event.Date}`;
+    eventp2.innerHTML = `${event.Time}`;
+    eventp3.innerHTML = `${event.Location}`;
+
+    eventBox.append(eventcard);
+    eventcard.appendChild(eventh3);
+    eventcard.appendChild(eventhr);
+    eventcard.appendChild(eventp1);
+    eventcard.appendChild(eventp2);
+    eventcard.appendChild(eventp3);
+  })
+});
   
-  
+
   
